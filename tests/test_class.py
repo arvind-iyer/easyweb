76ec7bf9ab06aec834ed95a6111162179231a51f
ngsp94 <ngsp1994@gmail.com>
2018-09-15 15:32:27 +0900

    added test class to test template with

---
+++ tests/test_class.py
@@ -0,0 +1,14 @@
+test_class = 
+{
+    "module_name": "mod_name",
+    "class_name": "class_name",
+    "methods":
+    [
+        {
+            "add": ["a", "b"],
+        },
+        {
+            "subtract": ["a", "b"],
+        }
+    ]
+}
