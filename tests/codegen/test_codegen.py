602dcdf3a6efbb2e68d6a9bfb926a347ee8b6fba
Camilo Sampedro <camilo.sampedro@g.hde.co.jp>
2018-09-15 16:25:26 +0900

    Basic test created for code generation

--- tests/codegen/test_codegen.py
+++ tests/codegen/test_codegen.py
@@ -1,5 +1,12 @@
 from easyweb.codegen.code_gen import create_code
+import os.path
 
 
 def test_template_reading():
-    pass
+    module_name = "my_easy_web_test"
+    class_name = "ClassExample"
+    methods = {
+        "my_method": ["a", "b"]
+    }
+    create_code(module_name, class_name, methods, "../template.py", "output.py")
+    assert(os.path.isfile("output.py"))
