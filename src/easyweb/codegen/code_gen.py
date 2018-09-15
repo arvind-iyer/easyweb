25ee8799e13a455632637a53ccd0338b9fa7acab
Camilo Sampedro <camilo.sampedro@g.hde.co.jp>
2018-09-15 15:22:46 +0900

    code gen file

---
+++ src/easyweb/codegen/code_gen.py
@@ -0,0 +1,28 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+env = Environment(
+    loader=PackageLoader('easy_web', 'templates')  # ,
+    #  autoescape=select_autoescape(['html', 'xml'])
+)
+
+template_file = "template.py"
+output_file_name = "flask_application.py"
+
+
+def create_code(module_name, class_name, methods):
+    template = get_template(template_file)
+    template_string = render_template(template, module_name, class_name, methods)
+    write(output_file_name, template_string)
+
+
+def get_template(template_name):
+    return env.get_template(template_name)
+
+
+def render_template(template, module_name, class_name, methods):
+    return template.render(module_name=module_name, class_name=class_name, methods=methods)
+
+
+def write(file_name, template_string):
+    with open(file_name, "w") as file:
+        file.write(template_string)
