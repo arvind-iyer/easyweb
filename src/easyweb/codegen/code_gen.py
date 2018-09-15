2a1244e480f6002f223ce5da9091d27f507e288c
Camilo A. Sampedro Restrepo <sampedro1903@gmail.com>
2018-09-15 17:09:42 +0900

    Feature code gen (#5)
    Generating code from template

---
+++ src/easyweb/codegen/code_gen.py
@@ -0,0 +1,28 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+env = Environment(
+    loader=PackageLoader('easyweb', 'templates')  # ,
+    #  autoescape=select_autoescape(['html', 'xml'])
+)
+
+# template_file = "template.py"
+# output_file_name = "flask_application.py"
+
+
+def create_code(data, template_file, output_file_name):
+    template = get_template(template_file)
+    template_string = render_template(template, data)
+    write(output_file_name, template_string)
+
+
+def get_template(template_name):
+    return env.get_template(template_name)
+
+
+def render_template(template, data):
+    return template.render(data=data)
+
+
+def write(file_name, template_string):
+    with open(file_name, "w") as file:
+        file.write(template_string)
