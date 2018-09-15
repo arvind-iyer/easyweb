1e11efad1323dfbb7534af99be9a8556fa25db42
Camilo Sampedro <camilo.sampedro@g.hde.co.jp>
2018-09-15 15:57:50 +0900

    Tests fixed

--- src/easyweb/codegen/code_gen.py
+++ src/easyweb/codegen/code_gen.py
@@ -1,7 +1,7 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 env = Environment(
-    loader=PackageLoader('easy_web', 'templates')  # ,
+    loader=PackageLoader('easyweb', 'templates')  # ,
     #  autoescape=select_autoescape(['html', 'xml'])
 )
 
