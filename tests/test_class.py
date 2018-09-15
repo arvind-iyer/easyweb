c6481c7b8f17c7a358e97044015a45d097202e0b
Arvind Iyer <arvindi.me@gmail.com>
2018-09-15 16:06:43 +0900

    Use a single dictionary instead of a list for the data structure

--- tests/test_class.py
+++ tests/test_class.py
@@ -3,12 +3,8 @@ test_class =
     "module_name": "mod_name",
     "class_name": "class_name",
     "methods":
-    [
-        {
+    {
             "add": ["a", "b"],
-        },
-        {
             "subtract": ["a", "b"],
-        }
-    ]
+    }
 }
