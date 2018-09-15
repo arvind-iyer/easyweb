ee5195c66a38880e28e8c86406109d53fa6885f4
terana <terana@lightning.local>
2018-09-15 17:11:55 +0900

    Changed user input

--- src/easyweb/templates/template.py
+++ src/easyweb/templates/template.py
@@ -2,13 +2,12 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 import {{ data['module_name'] }}
-{{ data['class_name'] }}_instance = {{ data['module_name'] }}.{{ data['class_name'] }}()
 
 {% for method_name in data['methods'] %}
 
 @app.route("/{{ method_name }}")
 def {{ method_name }}():
-	result = {{ data['class_name'] }}_instance.{{ method_name }}({{ ', '.join(data['methods'][method_name])}} )
+	result = {{ data['object'] }}.{{ method_name }}({{ ', '.join(data['methods'][method_name])}} )
 	return jsonify(result)
 
 {% endfor %}
