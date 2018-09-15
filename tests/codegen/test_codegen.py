e451acbd0bb7e45d46223044f73e5ea12d8426f4
Camilo Sampedro <camilo.sampedro@g.hde.co.jp>
2018-09-15 16:50:01 +0900

    Using single data variable in tests

--- tests/codegen/test_codegen.py
+++ tests/codegen/test_codegen.py
@@ -3,10 +3,12 @@ import os.path
 
 
 def test_template_reading():
-    module_name = "my_easy_web_test"
-    class_name = "ClassExample"
-    methods = {
-        "my_method": ["a", "b"]
+    data = {
+        "module_name": "my_easy_web_test",
+        "class_name": "ClassExample",
+        "methods": {
+            "my_method": ["a", "b"]
+        }
     }
-    create_code(module_name, class_name, methods, "../template.py", "output.py")
+    create_code(data, "template.py", "output.py")
     assert(os.path.isfile("output.py"))
